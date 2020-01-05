9bd9952c5fcd0ad6e41abebdbcfb898944216971
Amit Mundra <amitmundra@Amits-MacBook-Air.local>
2020-01-05 17:15:18 +0530

    fxing audio

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
         super(props);
         this.audio = new Audio(soundfile);
     }
-    componentWillMount() {
+    componentDidMount() {
         this.audio.play();
         this.audio.addEventListener(
             "ended",
